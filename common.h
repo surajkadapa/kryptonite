38f9a690a2f2359b62a045ddfd0ed974a0793923
Suraj Kadapa <surajkadapa103@gmail.com>
2025-02-04 22:35:34 +0530

    added panic and few stdlib functions

--- common.h
+++ common.h
@@ -23,5 +23,6 @@ typedef uint32_t vaddr_t; //to represent virtual memory address
 void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 char *strcpy(char *dst, const char *src);
+char *strcpy_s(char *dst, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
--- kernel.h
+++ kernel.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#define PANIC(fmt, ...) \
+	do{ \
+		printf("KERNEL_PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+		while(1){} \
+	} while (0) //this method to make sure the statements here execute atleast once
+
 struct sbiret {
 	long error;
 	long value;
 };
+
+
